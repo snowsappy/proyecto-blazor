c77a8784f605a3744fb797aa05f78eab1520acdd
Andres <feliandres412@gmail.com>
2026-08-14 16:32:39 -0500

    h

---
+++ Tiendamascotad/Tiendamascotad/Components/Models/Mascota.cs
@@ -0,0 +1,11 @@
+﻿namespace Tiendamascotad.Components.Models
+{
+     public class Mascota
+    {
+
+        public string nombre { get; set; }
+        public string imagen { get; set; }
+        public string especie { get; set; }
+        public int edad { get; set; }
+    }
+};
